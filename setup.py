a7c57b492d753091dea7eb967274a36a3827eaa5
DESKTOP-FAR85NI\user <mohammad.dowlut5@umail.uom.ac.mu>
2024-07-28 12:54:08 +0400

    Second

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='NutriGuideChatbot',
+    version='0.0.0',
+    author='Yahya',
+    author_email='yahyahus02@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
